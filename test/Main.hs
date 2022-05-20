8c60253733f4033fa18421bbc38b8844d7e93f43
Seungheon Oh <seungheon.ooh@gmail.com>
2022-05-20 11:58:25 -0500

    property testing

--- test/Main.hs
+++ test/Main.hs
@@ -1,42 +1,46 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Main (main) where
 
+import Plutarch.Context.Config
 import Plutarch.Context.Spending
 import Plutus.V1.Ledger.Api
 import Plutus.V1.Ledger.Value
 
-import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty (adjustOption, defaultMain, testGroup)
 import Test.Tasty.QuickCheck
 
 import Control.Applicative
-import Data.ByteString.Char8 qualified as C
+import qualified Data.ByteString.Char8 as C
 import Data.ByteString.Hash (sha2)
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
 
 data SpendingBuilderElements datum
-  = InputFromPubKey PubKeyHash Value
-  | InputFromPubKeyWith PubKeyHash Value ()
-  | InputFromOtherScript ValidatorHash Value ()
-  | InputSelfExtra Value datum
-  | OutputToPubKey PubKeyHash Value
-  | OutputToPubKeyWith PubKeyHash Value ()
-  | OutputToOtherScript ValidatorHash Value ()
-  | OutputToValidator Value datum
-  | Mint Value
-  | SignedWith PubKeyHash
-  | ExtraData ()
-  deriving stock (Show)
+    = InputFromPubKey PubKeyHash Value
+    | InputFromPubKeyWith PubKeyHash Value ()
+    | InputFromOtherScript ValidatorHash Value ()
+    | InputSelfExtra Value datum
+    | OutputToPubKey PubKeyHash Value
+    | OutputToPubKeyWith PubKeyHash Value ()
+    | OutputToOtherScript ValidatorHash Value ()
+    | OutputToValidator Value datum
+    | Mint Value
+    | SignedWith PubKeyHash
+    | ExtraData ()
+    deriving stock (Show)
 
-mkBuilderElement :: forall datum redeemer. SpendingBuilderElements datum -> SpendingBuilder datum redeemer
-mkBuilderElement (InputFromPubKey pk val) = inputFromPubKey pk val
-mkBuilderElement (InputFromPubKeyWith pk val ()) = inputFromPubKeyWith pk val ()
-mkBuilderElement (InputFromOtherScript valhash val ()) = inputFromOtherScript valhash val ()
-mkBuilderElement (InputSelfExtra val datum) = inputSelfExtra val datum
-mkBuilderElement (OutputToPubKey pk val) = outputToPubKey pk val
-mkBuilderElement (OutputToPubKeyWith pk val ()) = outputToPubKeyWith pk val ()
-mkBuilderElement (OutputToOtherScript valhash val ()) = outputToOtherScript valhash val ()
-mkBuilderElement (OutputToValidator val datum) = outputToValidator val datum
-mkBuilderElement (Mint val) = mint val
-mkBuilderElement (SignedWith pk) = signedWith pk
-mkBuilderElement (ExtraData ()) = extraData ()
+toSpendingBuilder :: forall datum redeemer. SpendingBuilderElements datum -> SpendingBuilder datum redeemer
+toSpendingBuilder (InputFromPubKey pk val) = inputFromPubKey pk val
+toSpendingBuilder (InputFromPubKeyWith pk val ()) = inputFromPubKeyWith pk val ()
+toSpendingBuilder (InputFromOtherScript valhash val ()) = inputFromOtherScript valhash val ()
+toSpendingBuilder (InputSelfExtra val datum) = inputSelfExtra val datum
+toSpendingBuilder (OutputToPubKey pk val) = outputToPubKey pk val
+toSpendingBuilder (OutputToPubKeyWith pk val ()) = outputToPubKeyWith pk val ()
+toSpendingBuilder (OutputToOtherScript valhash val ()) = outputToOtherScript valhash val ()
+toSpendingBuilder (OutputToValidator val datum) = outputToValidator val datum
+toSpendingBuilder (Mint val) = mint val
+toSpendingBuilder (SignedWith pk) = signedWith pk
+toSpendingBuilder (ExtraData ()) = extraData ()
 
 genHashByteString :: Gen C.ByteString
 genHashByteString = sha2 . C.pack . show <$> (chooseAny :: Gen Integer)
@@ -49,42 +53,87 @@ genPrettyByteString = C.pack <$> (listOf1 $ elements ['a' .. 'z'])
 
 genAssetClass :: Gen AssetClass
 genAssetClass =
-  AssetClass
-    <$> liftA2
-      (,)
-      (currencySymbol <$> genHashByteString)
-      (tokenName <$> genPrettyByteString)
+    AssetClass
+        <$> liftA2
+            (,)
+            (currencySymbol <$> genHashByteString)
+            (tokenName <$> genPrettyByteString)
 
 genAnyValue :: Gen Value
 genAnyValue = genAssetClass >>= genValue
 
-genBuilderElement :: Gen (SpendingBuilderElements ())
-genBuilderElement = do
-  pk <- PubKeyHash . toBuiltin <$> genHashByteString
-  valhash <- ValidatorHash . toBuiltin <$> genHashByteString
-  val <- genAnyValue
+genBuilderElements :: Gen ([SpendingBuilderElements ()], ValidatorUTXO ())
+genBuilderElements = (,) <$> listOf1 (genElement) <*> (genAnyValue >>= pure . ValidatorUTXO ())
+  where
+    genElement = do
+        pk <- PubKeyHash . toBuiltin <$> genHashByteString
+        valhash <- ValidatorHash . toBuiltin <$> genHashByteString
+        val <- genAnyValue
 
-  elements [ InputFromPubKey pk val
-           , InputFromPubKeyWith pk val ()
-           , InputFromOtherScript valhash val ()
-           , InputSelfExtra val ()
-           , OutputToPubKey pk val
-           , OutputToPubKeyWith pk val ()
-           , OutputToOtherScript valhash val ()
-           , OutputToValidator val ()
-           , Mint val
-           , SignedWith pk
-           , ExtraData ()
-           ]
+        elements
+            [ InputFromPubKey pk val
+            , InputFromPubKeyWith pk val ()
+            , InputFromOtherScript valhash val ()
+            , InputSelfExtra val ()
+            , OutputToPubKey pk val
+            , OutputToPubKeyWith pk val ()
+            , OutputToOtherScript valhash val ()
+            , OutputToValidator val ()
+            , Mint val
+            , SignedWith pk
+            , ExtraData ()
+            ]
 
-_used :: Gen (SpendingBuilderElements ())
-_used = genBuilderElement
+buildContext :: ([SpendingBuilderElements ()], ValidatorUTXO ()) -> Maybe ScriptContext
+buildContext (xs, vutxo) = spendingContext defaultConfig builder vutxo
+  where
+    builder = foldr1 (<>) $ (toSpendingBuilder <$> xs)
 
-_used2 :: forall datum redeemer. SpendingBuilder datum redeemer
-_used2 = mkBuilderElement undefined
+rules :: ScriptContext -> SpendingBuilderElements () -> Bool
+rules context spe = go spe
+  where
+    addrValPairs = fmap (\out -> (txOutAddress out, txOutValue out))
+    inAddrVal = addrValPairs $ txInInfoResolved <$> (txInfoInputs . scriptContextTxInfo $ context)
+    outAddrVal = addrValPairs $ txInfoOutputs . scriptContextTxInfo $ context
+    pkToAddr = (flip Address Nothing) . PubKeyCredential
+    vhashToAddr = (flip Address Nothing) . ScriptCredential
+
+    go (InputFromPubKey (pkToAddr -> addr) val) =
+        elem (addr, val) inAddrVal
+    go (InputFromPubKeyWith (pkToAddr -> addr) val ()) =
+        elem (addr, val) inAddrVal
+    go (InputFromOtherScript (vhashToAddr -> addr) val ()) =
+        elem (addr, val) inAddrVal
+    go (InputSelfExtra val datum) =
+        elem (vhashToAddr . configValidatorHash $ defaultConfig, val) inAddrVal
+    go (OutputToPubKey (pkToAddr -> addr) val) =
+        elem (addr, val) outAddrVal
+    go (OutputToPubKeyWith (pkToAddr -> addr) val ()) =
+        elem (addr, val) outAddrVal
+    go (OutputToOtherScript (vhashToAddr -> addr) val ()) =
+        elem (addr, val) outAddrVal
+    go (OutputToValidator val datum) =
+        elem (vhashToAddr . configValidatorHash $ defaultConfig, val) outAddrVal
+    go (Mint val) = True
+    go (SignedWith pk) =
+        let signers = txInfoSignatories . scriptContextTxInfo $ context
+         in elem pk signers
+    go (ExtraData ()) = True
+
+prop :: Property
+prop = forAll genBuilderElements go
+  where
+    go elms = case buildContext elms of
+        Nothing -> False
+        Just context -> foldr1 (&&) $ rules context <$> fst elms
 
 main :: IO ()
-main = defaultMain
-  $ testGroup "context builder"
-  [
-  ]
+main = do
+    setLocaleEncoding utf8
+    defaultMain . adjustOption go $
+        testGroup
+            "context builder"
+            [testProperty "builder builds all inputs correctly" prop]
+  where
+    go :: QuickCheckTests -> QuickCheckTests
+    go = max 10_000

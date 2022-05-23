6708da11a5e01b3731dd52e899f016add2c1a346
Seungheon Oh <seungheon.ooh@gmail.com>
2022-05-22 21:12:15 -0500

    using `Integer` for datum.

--- test/Main.hs
+++ test/Main.hs
@@ -20,33 +20,33 @@ import Debug.Trace
 
 -- SpendingBuilderElements is a one to one representation of Spending
 -- Builder API. 
-data SpendingBuilderElements datum
+data SpendingBuilderElements
     = InputFromPubKey PubKeyHash Value
-    | InputFromPubKeyWith PubKeyHash Value ()
-    | InputFromOtherScript ValidatorHash Value ()
-    | InputSelfExtra Value datum
+    | InputFromPubKeyWith PubKeyHash Value Integer
+    | InputFromOtherScript ValidatorHash Value Integer
+    | InputSelfExtra Value Integer
     | OutputToPubKey PubKeyHash Value
-    | OutputToPubKeyWith PubKeyHash Value ()
-    | OutputToOtherScript ValidatorHash Value ()
-    | OutputToValidator Value datum
+    | OutputToPubKeyWith PubKeyHash Value Integer
+    | OutputToOtherScript ValidatorHash Value Integer
+    | OutputToValidator Value Integer
     | Mint Value
     | SignedWith PubKeyHash
-    | ExtraData ()
+    | ExtraData Integer
     deriving stock (Show)
 
 -- Translate API representation into actual API.
-toSpendingBuilder :: forall datum redeemer. SpendingBuilderElements datum -> SpendingBuilder datum redeemer
+toSpendingBuilder :: forall redeemer. SpendingBuilderElements -> SpendingBuilder Integer redeemer
 toSpendingBuilder (InputFromPubKey pk val) = inputFromPubKey pk val
-toSpendingBuilder (InputFromPubKeyWith pk val ()) = inputFromPubKeyWith pk val ()
-toSpendingBuilder (InputFromOtherScript valhash val ()) = inputFromOtherScript valhash val ()
-toSpendingBuilder (InputSelfExtra val datum) = inputSelfExtra val datum
+toSpendingBuilder (InputFromPubKeyWith pk val dat) = inputFromPubKeyWith pk val dat
+toSpendingBuilder (InputFromOtherScript valhash val dat) = inputFromOtherScript valhash val dat
+toSpendingBuilder (InputSelfExtra val dat) = inputSelfExtra val dat
 toSpendingBuilder (OutputToPubKey pk val) = outputToPubKey pk val
-toSpendingBuilder (OutputToPubKeyWith pk val ()) = outputToPubKeyWith pk val ()
-toSpendingBuilder (OutputToOtherScript valhash val ()) = outputToOtherScript valhash val ()
-toSpendingBuilder (OutputToValidator val datum) = outputToValidator val datum
+toSpendingBuilder (OutputToPubKeyWith pk val dat) = outputToPubKeyWith pk val dat
+toSpendingBuilder (OutputToOtherScript valhash val dat) = outputToOtherScript valhash val dat
+toSpendingBuilder (OutputToValidator val dat) = outputToValidator val dat
 toSpendingBuilder (Mint val) = mint val
 toSpendingBuilder (SignedWith pk) = signedWith pk
-toSpendingBuilder (ExtraData ()) = extraData ()
+toSpendingBuilder (ExtraData dat) = extraData dat
 
 genHashByteString :: Gen C.ByteString
 genHashByteString = sha2 . C.pack . show <$> (chooseAny :: Gen Integer)
@@ -70,48 +70,48 @@ genAnyValue = genAssetClass >>= genValue
 
 -- Arbitrary instance for SpendingBuilderElements with Unit as datum.
 -- It is required to use the free-shrinker of `Arbirary a => Arbitrary (List a)`
-instance Arbitrary (SpendingBuilderElements ()) where
+instance Arbitrary SpendingBuilderElements where
     arbitrary = do
       -- These shouldn't effect performance since these are Lazy. I think...
       pk <- PubKeyHash . toBuiltin <$> genHashByteString
       valhash <- ValidatorHash . toBuiltin <$> genHashByteString
       val <- genAnyValue
+      datum <- arbitrary
 
       elements
         [ InputFromPubKey pk val
-        , InputFromPubKeyWith pk val ()
-        , InputFromOtherScript valhash val ()
-        , InputSelfExtra val ()
+        , InputFromPubKeyWith pk val datum
+        , InputFromOtherScript valhash val datum
+        , InputSelfExtra val datum
         , OutputToPubKey pk val
-        , OutputToPubKeyWith pk val ()
-        , OutputToOtherScript valhash val ()
-        , OutputToValidator val ()
+        , OutputToPubKeyWith pk val datum
+        , OutputToOtherScript valhash val datum
+        , OutputToValidator val datum
         , Mint val
         , SignedWith pk
-        , ExtraData ()
+        , ExtraData datum
         ]
 
 -- Generates tuple of required information needed for ScriptContext generation.
-genBuilderElements :: Gen ([SpendingBuilderElements ()], ValidatorUTXO ())
-
-genBuilderElements = (,) <$> listOf1 arbitrary <*> (genAnyValue >>= pure . ValidatorUTXO ())
+genBuilderElements :: Gen ([SpendingBuilderElements], ValidatorUTXO Integer)
+genBuilderElements = (,) <$> listOf1 arbitrary <*> (genAnyValue >>= pure . ValidatorUTXO 0)
 
 -- shrinker for BuilderElements generator, it only uses the free-shrinker of list.
 shrinkBuilderElements ::
-    ([SpendingBuilderElements ()], ValidatorUTXO ()) ->
-    [([SpendingBuilderElements ()], ValidatorUTXO ())]
+    ([SpendingBuilderElements], ValidatorUTXO Integer) ->
+    [([SpendingBuilderElements], ValidatorUTXO Integer)]
 shrinkBuilderElements (elms, v) = (,v) <$> shrink elms
 
 -- build ScriptContext from given list of SpendingBuilderElements and
 -- ValidatorUTXO--generator outputs--with default configuration.
-buildContext :: ([SpendingBuilderElements ()], ValidatorUTXO ()) -> Maybe ScriptContext
+buildContext :: ([SpendingBuilderElements], ValidatorUTXO Integer) -> Maybe ScriptContext
 buildContext (xs, vutxo) = spendingContext defaultConfig builder vutxo
   where
     builder = mconcat $ toSpendingBuilder <$> xs
 
 -- Check if given SpendingBuilderElements is correctly represented
 -- in the ScriptContext. Return True if it's correct, False otherwise.
-rules :: ScriptContext -> SpendingBuilderElements () -> Bool
+rules :: ScriptContext -> SpendingBuilderElements -> Bool
 rules context spe = go spe
   where
     addrValPairs = fmap (\out -> (txOutAddress out, txOutValue out))
@@ -125,15 +125,15 @@ rules context spe = go spe
     go (InputFromPubKeyWith (pkToAddr -> addr) val dat) =
       let dh = datumHash . Datum . toBuiltinData $ dat in
         trace (show dh) $ elem (addr, val) inAddrVal
-    go (InputFromOtherScript (vhashToAddr -> addr) val ()) =
+    go (InputFromOtherScript (vhashToAddr -> addr) val _int) =
         elem (addr, val) inAddrVal
     go (InputSelfExtra val _datum) =
         elem (vhashToAddr . configValidatorHash $ defaultConfig, val) inAddrVal
     go (OutputToPubKey (pkToAddr -> addr) val) =
         elem (addr, val) outAddrVal
-    go (OutputToPubKeyWith (pkToAddr -> addr) val ()) =
+    go (OutputToPubKeyWith (pkToAddr -> addr) val _int) =
         elem (addr, val) outAddrVal
-    go (OutputToOtherScript (vhashToAddr -> addr) val ()) =
+    go (OutputToOtherScript (vhashToAddr -> addr) val _int) =
         elem (addr, val) outAddrVal
     go (OutputToValidator val _datum) =
         elem (vhashToAddr . configValidatorHash $ defaultConfig, val) outAddrVal
@@ -141,7 +141,7 @@ rules context spe = go spe
     go (SignedWith pk) =
         let signers = txInfoSignatories . scriptContextTxInfo $ context
          in elem pk signers
-    go (ExtraData ()) = True
+    go (ExtraData _int) = True
 
 correctInputsAndOutputs :: Property
 correctInputsAndOutputs = forAllShrink genBuilderElements shrinkBuilderElements go
